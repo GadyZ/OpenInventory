0cdb72b30e82e40d5b06236bb9f3ca5a30fc5c09
Bejasc <benjamin.schulz94@gmail.com>
2018-06-13 22:08:23 +0930

    Inventory.cs attempt

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,84 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour {
 
-    public List<Slot> slots = new List<Slot>();
+    public static UIController instance;
+    private void OnEnable()
+    {
+        if(instance == null)
+            instance = this;
+
+        DontDestroyOnLoad(this);
+    }
+
+    public Slot emptySlot;
+    public GridLayoutGroup slotHolder;  
+
+    public Inventory playerInventory;
+    public int playerInventorySlots = 10;
 
     public Item item;
     public int qty;
 
+    private void Awake()
+    {
+        playerInventory = new Inventory(playerInventorySlots);
+
+        DrawInventory(playerInventory);
+    }
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
-            AddItem(item, qty);
+            playerInventory.AddItem(item, qty);
 
         if (Input.GetKeyDown(KeyCode.Backspace))
-            RemoveItem(item, qty);
+            playerInventory.RemoveItem(item, qty);
     }
 
-    public void AddItem(Item item, int qty)
+    void DrawInventory(Inventory inventory)
     {
-        Debug.Log("Adding " + qty + " " + item.name);
-        while(qty > 0)
+        foreach (Transform t in slotHolder.transform)
+            Destroy(t.gameObject);
+
+        foreach (var x in inventory.slots)
         {
-            if(slots.Exists(x=> (x.itemInSlot == item) && x.currentStack < item.maxStack))
+            var newSlot = Instantiate(emptySlot);
+            newSlot.transform.SetParent(slotHolder.transform);
+
+            if (x.itemInSlot != null)
             {
-                var targetSlot = slots.First((x => (x.itemInSlot == item) && x.currentStack < item.maxStack));
-                var amountToAdd = Mathf.Min(qty, item.maxStack - targetSlot.currentStack);
-                targetSlot.currentStack += amountToAdd;
-                targetSlot.RedrawSlot();
-                qty -= amountToAdd;
+                newSlot.itemInSlot = x.itemInSlot;
+                newSlot.currentStack = x.currentStack;
             }
             else
             {
-                if(slots.Exists(x => x.itemInSlot == null))
-                {
-                    var targetSlot = slots.First(x => x.itemInSlot == null);
-                    targetSlot.itemInSlot = item;
-                    targetSlot.currentStack = 0;
-                }
-                else
-                {
-                    Debug.Log(qty + " " + item.name + " could not be added");
-                    break;
-                }
+                newSlot.itemInSlot = null;
+                newSlot.currentStack = 0;
             }
+
+            newSlot.RedrawSlot();
         }
-
-        Debug.Log("Player has " + GetItemCount(item) + " total " + item.name);
     }
-
-    public void RemoveItem(Item item, int qty)
-    {
-        var itemCount = GetItemCount(item);
-        if (itemCount >= qty)
-        {
-            slots.OrderByDescending(x => x.currentStack);
-            while (qty > 0)
-            {
-                var targetSlot = slots.Last(x => (x.itemInSlot == item) && x.currentStack > 0);
-                var amountToRemove = Mathf.Min(qty, targetSlot.currentStack);
-
-                targetSlot.currentStack -= amountToRemove;
-                if (targetSlot.currentStack == 0)
-                    targetSlot.itemInSlot = null;
-
-                targetSlot.RedrawSlot();
-                qty -= amountToRemove;
-            }
-        }
-        else
-        {
-            Debug.Log("Not enough " + item.name + " exists to remove " + qty+". Has "+itemCount);
-        }
-
-        Debug.Log("Player has " + GetItemCount(item) + " total " + item.name);
-    }
-
-    public int GetItemCount(Item item)
-    {
-        return slots.Where(x => x.itemInSlot == item).Sum(x => x.currentStack);
-    }
+    
 }
